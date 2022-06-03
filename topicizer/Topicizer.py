d0bbbda43e04c6012fab3b303b9e8579a7f742aa
Thedon989 <nick.patrick.adams@gmail.com>
2022-06-03 14:15:11 -0700

    Started filling out Topicizer.py functions to be used by the TopicizerCommands.py

--- topicizer/Topicizer.py
+++ topicizer/Topicizer.py
@@ -1,44 +1,65 @@
 from discord.ext import commands
 from util.FileIO import FileIO
 from util.Settings import Settings
+import random
 import dotenv
 
 
 class Topicizer:
     def __init__(self):
         dotenv.load_dotenv(dotenv_path="app/.env")
-        self.bot = self.spawn_bot()
+        self.bot = self.__spawn_bot()
 
-        self.__topics = self.load()
+        self.__topics = self.__load()
         self.__trivia_category_mode = True
-        self.__current_category = ""
-        self.__current_topic = ""
-        self.__current_trivia = ""
+        self.__current_category = self.fetch_category("random")
+        self.__current_topic = self.fetch_topic()
+        self.__current_trivia = self.fetch_trivia()
         self.__answer = ""
 
     def add_topic(self, topic):
-        ""
+        self.__topics[self.__current_category]['Topic'][topic] = {}
 
     def fetch_topic(self):
-        ""
+        fetched_topic = random.choice(list(self.__topics[self.__current_category]['Topic']))
+        self.__current_topic = fetched_topic
+        return fetched_topic
 
-    def add_trivia(self,trivia):
-        ""
+    def add_trivia(self, trivia, answer):
+        if self.__trivia_category_mode:
+            self.__topics[self.__current_category]['Trivia'][trivia] = answer
+        else:
+            self.__topics[self.__current_category]['Topic'][self.__current_topic] += {trivia: answer}
 
     def fetch_trivia(self):
-        ""
+        if self.__trivia_category_mode:
+            fetched_trivia = random.choice(list(self.__topics[self.__current_category]['Trivia']))
+        else:
+            fetched_trivia = random.choice(list(self.__topics[self.__current_category]['Topic'][self.__current_topic]))
+        self.__current_trivia = fetched_trivia
+        return fetched_trivia
 
     def toggle_trivia_mode(self):
         self.__trivia_category_mode = not self.__trivia_category_mode
 
     def add_category(self, category):
-        ""
+        self.__topics[category] = {"Topic": {}, "Trivia": {}}
 
     def fetch_category(self, category):
-        ""
+        fetched_category = "The category you entered does not exist\n\
+                            Use command !category add \"{category}\" to add this category as a new one\n\
+                            Use command !category \"random\" to get a random category\n\
+                            Or use command !category list to get a list of all categories".format(category=category)
+
+        if category.lower() == "random":
+            fetched_category = random.choice(list(self.__topics))
+        elif self.__topics.get(category) != None:
+            fetched_category = category
+        self.__current_category = fetched_category
+        return fetched_category
 
     def list_category(self):
-        ""
+        return self.topics.keys()
 
     @staticmethod
     def __load():
--- topicizer/TopicizerCommands.py
+++ topicizer/TopicizerCommands.py
@@ -7,14 +7,14 @@ def TopicizerCommands(topicizer):
                          help="Allows Users To Display Current Topic\n\
                                !topic")
     async def topic(ctx):
-        response = "tesst"
+        response = "Current Topic is: " + topicizer.get_topic()
         await ctx.send(response)
 
     @topic.command(name='fetch',
                    help="Sets A New Current Topic Randomly\n\
                          !topic fetch")
     async def fetch_topic(ctx):
-        response = "tesst"
+        response = "New Topic is :" + topicizer.fetch_topic()
         await ctx.send(response)
 
     @topic.command(name='add',
@@ -22,7 +22,8 @@ def TopicizerCommands(topicizer):
                          !topic add \"topic\"\n\
                          Example: !topic add \"Search for life on Mars\"")
     async def add_topic(ctx, topic):
-        response = "tesst"
+        topicizer.add_topic(topic)
+        response = topic + " added to Topics"
         await ctx.send(response)
 
 ### Trivia Commands ######################################################################
@@ -31,14 +32,14 @@ def TopicizerCommands(topicizer):
                          help="Allows Users To Display Current Trivia\n\
                                !trivia")
     async def trivia(ctx):
-        response = "tesst1"
+        response = "Current Trivia is: " + topicizer.get_trivia()
         await ctx.send(response)
 
     @trivia.command(name='fetch',
                     help="Sets A New Current Trivia Randomly\n\
                           !trivia fetch")
     async def fetch_trivia(ctx):
-        response = "tesst"
+        response = "New Trivia is" + topicizer.fetch_trivia()
         await ctx.send(response)
 
     @trivia.command(name='add',
@@ -46,9 +47,8 @@ def TopicizerCommands(topicizer):
                           !trivia add \"trivia\" \"answer\"\n\
                           Example: !trivia add \"What year did Curiosity Rover land on Mars?\" \"2012\"")
     async def add_trivia(ctx, trivia, answer):
-        print(trivia)
-        print(answer)
-        response = "tesst2"
+        topicizer.add_trivia(trivia, answer)
+        response = trivia + " Trivia added with answer " + answer
         await ctx.send(response)
 
     @trivia.command(name='mode',
@@ -56,30 +56,34 @@ def TopicizerCommands(topicizer):
                           By Default Mode Is Set To Category\n\
                           !trivia mode")
     async def switch_mode(ctx):
-        print("hi")
+        topicizer.toggle_trivia_mode()
+        response = "Trivia mode toggled"
+        await ctx.send(response)
 
 ### Category Commands ######################################################################
 
     @topicizer.bot.group(name='category', invoke_without_command=True,
                          help="Allows Users To Specify Category\n\
                                !category \"category\"\n\
-                               Example: !category \"Space\"\n\
-                               Call !category To Display Current Category")
+                               Example: !category \"Space\"")
     async def category(ctx, category):
-        response = "tesst"
+        response = "New Category is: " + topicizer.fetch_category(category)
         await ctx.send(response)
 
     @category.command(name="add", help="Allows Users To Add New Categories\n\
                                        !category add \"category\"\n\
                                        Example: !category add \"Geography\"")
     async def add_category(ctx, category):
-        print("")
+        topicizer.add_category(category)
+        response = category + " has been added to Categories"
+        await ctx.send(response)
 
     @category.command(name="list",
                       help="Allows Users To List All Categories\n\
                             !category list\n")
     async def list_categories(ctx):
-        print("")
+        response = "List of Categories is: " + topicizer.list_category()
+        await ctx.send(response)
 
 ### Bot Events ######################################################################
 
